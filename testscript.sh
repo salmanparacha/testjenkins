5b138d12062b1970277b63ff402f6dbf7f4565a3
Salman Paracha <sparacha@FreedomMobile.ca>
2020-01-08 12:37:00 -0500

    testing webhook

--- testscript.sh
+++ testscript.sh
@@ -3,4 +3,5 @@ echo "this is the first test jenkins script"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "This job is done"
+echo "Ths is to test whether github webhook is working or now"
 
